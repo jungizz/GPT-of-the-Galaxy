b89fe43acf60d1cc66689fdfff6af58a1841fb20
GaeunU <65946325+gaeunU@users.noreply.github.com>
2023-07-16 19:34:38 +0900

    Mars Gravity

--- Assets/Scripts/Mars/Mars_Button.cs
+++ Assets/Scripts/Mars/Mars_Button.cs
@@ -20,5 +20,19 @@ public class Mars_Button : MonoBehaviour
     public void Intro_Button()
     {
         GameObject.Find("Mars_Canvas").transform.GetChild(0).gameObject.SetActive(false);
+        StartCoroutine(WaitAndPrint(2.0f));
+    }
+
+    IEnumerator WaitAndPrint(float waitTime)
+    {
+        
+        yield return new WaitForSeconds(waitTime);
+        GameObject.Find("Mars_Canvas").transform.GetChild(1).gameObject.SetActive(true);
+        Debug.Log("2초가 지났습니다");
+    }
+
+    public void Talk_Button()
+    {
+        GameObject.Find("Mars_Canvas").transform.GetChild(1).gameObject.SetActive(false);
     }
 }
