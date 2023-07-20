40cd2c140374248af150db046b55b7f51bd9dbf4
jungizz <113113802+jungizz@users.noreply.github.com>
2023-07-21 05:26:10 +0900

    Edit Space Scene
    - rotation, orbit of planet
    - edit material

---
+++ Assets/Scripts/SpinPlanet.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPlanet : MonoBehaviour
+{
+    public float selfRotateSpeed; //자전 각속도
+
+    public float orbitRotateSpeed; //공전 각속도
+    private float rotateRadius; //회전 반지름
+    private float rotateAngle; //회전 각도
+
+    void Start()
+    {
+        rotateRadius = transform.localPosition.x; //원점(태양)으로부터의 거리
+    }
+
+
+    void Update()
+    {
+        transform.Rotate(0, selfRotateSpeed * Time.deltaTime, 0); //자전
+
+        rotateAngle += Time.deltaTime * orbitRotateSpeed;
+        if(rotateAngle < 360)
+        {
+            float xPos = rotateRadius * Mathf.Sin(rotateAngle);
+            float zPos = rotateRadius * Mathf.Cos(rotateAngle);
+
+            //if (gameObject.name == "Moon")
+
+            transform.localPosition = new Vector3(xPos, 0, zPos);
+        }
+        
+    }
+}
