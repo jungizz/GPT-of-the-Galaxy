b7d26b63b9b0a43e4c165171b651c32d214a5d34
GaeunU <65946325+gaeunU@users.noreply.github.com>
2023-07-29 23:22:16 +0900

    Add Godot Button

--- Assets/Scripts/AI/Godot.cs
+++ Assets/Scripts/AI/Godot.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Godot : MonoBehaviour
 {
-    public bool Godotbuttonflag = false;
-    public GameObject button;
+    public GameObject Qestion_button;
+    public GameObject Back_button;
+    public GameObject Close_button;
+    public GameObject GPTUI;
 
     // Start is called before the first frame update
     void Start()
@@ -16,16 +19,35 @@ public class Godot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         
     }
 
-    public void GodotPop()
+    public void GodotPopup()
     {
-        Godotbuttonflag = !Godotbuttonflag;
+        Qestion_button.SetActive(true);
+        Back_button.SetActive(true);
+        Close_button.SetActive(true);
 
-        if (Godotbuttonflag)
-            button.SetActive(true);
-        else
-            button.SetActive(false);
+    }
+
+    public void GodotPopdown()
+    {
+        Qestion_button.SetActive(false);
+        Back_button.SetActive(false);
+        Close_button.SetActive(false);
+        GPTUI.SetActive(false);
+    }
+
+    public void backtospace()
+    {
+        SceneManager.LoadScene("Space");   
+    }
+
+    public void GPTUIup()
+    {
+        Qestion_button.SetActive(false);
+        Back_button.SetActive(false);
+        GPTUI.SetActive(true);
     }
 }
