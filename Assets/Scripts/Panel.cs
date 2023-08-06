711d95a19f850edda0093e7ec18c4c752abe2fd0
배 현아 <gusdk3312@ajou.ac.kr>
2023-08-06 19:48:26 +0900

    add : planet panel

--- Assets/Scripts/Panel.cs
+++ Assets/Scripts/Panel.cs
@@ -8,9 +8,24 @@ public class Panel : MonoBehaviour
     public GameObject panel;
     public GameObject infoPanel1;
     public GameObject infoPanel2;
+
     public GameObject info_info;
     public GameObject info_info2;
     public GameObject info_info3;
+    public GameObject info_info4;
+    public GameObject info_info5;
+    public GameObject info_info6;
+
+    public GameObject next;
+    public GameObject next2;
+
+    public GameObject info2_info;
+    public GameObject info2_info2;
+    public GameObject info2_info3;
+
+    public GameObject info2_Panel;
+    public GameObject info2_Panel2;
+    public GameObject info2_Panel3;
 
     public AudioSource sound;
 
@@ -28,6 +43,22 @@ public class Panel : MonoBehaviour
         sound.Play();
     }
 
+    public void NextButton()
+    {
+        next.SetActive(true);
+        next2.SetActive(false);
+        sound.Play();
+    }
+
+    public void NextButton2()
+    {
+        next.SetActive(false);
+        next2.SetActive(true);
+        sound.Play();
+    }
+
+    //패널 여는 기능
+
     public void OpenInfoPanel1()
     {
         panel.SetActive(false);
@@ -42,11 +73,42 @@ public class Panel : MonoBehaviour
         sound.Play();
     }
 
+    //내부구조에서 세부 정보 패널 여는 기능
+    public void OpenInfo2_Panel()
+    {
+        infoPanel2.SetActive(false);
+        info2_Panel.SetActive(true);
+        sound.Play();
+    }
+
+    public void OpenInfo2_Panel2()
+    {
+        infoPanel2.SetActive(false);
+        info2_Panel2.SetActive(true);
+        sound.Play();
+    }
+
+    public void OpenInfo2_Panel3()
+    {
+        infoPanel2.SetActive(false);
+        info2_Panel3.SetActive(true);
+        sound.Play();
+    }
+
+    //텍스트 여는 기능
+
     public void OpenInfo_Info1()
     {
         info_info.SetActive(true);
         info_info2.SetActive(false);
-        info_info3.SetActive(false);
+        if (info_info3 != null)
+            info_info3.SetActive(false);
+        if (info_info4 != null)
+            info_info4.SetActive(false);
+        if (info_info5 != null)
+            info_info5.SetActive(false);
+        if (info_info6 != null)
+            info_info6.SetActive(false);
         sound.Play();
     }
 
@@ -54,7 +116,14 @@ public class Panel : MonoBehaviour
     {
         info_info.SetActive(false);
         info_info2.SetActive(true);
-        info_info3.SetActive(false);
+        if (info_info3 != null)
+            info_info3.SetActive(false);
+        if (info_info4 != null)
+            info_info4.SetActive(false);
+        if (info_info5 != null)
+            info_info5.SetActive(false);
+        if (info_info6 != null)
+            info_info6.SetActive(false);
         sound.Play();
     }
 
@@ -62,9 +131,88 @@ public class Panel : MonoBehaviour
     {
         info_info.SetActive(false);
         info_info2.SetActive(false);
-        info_info3.SetActive(true);
+        if (info_info3 != null)
+            info_info3.SetActive(true);
+        if (info_info4 != null)
+            info_info4.SetActive(false);
+        if (info_info5 != null)
+            info_info5.SetActive(false);
+        if (info_info6 != null)
+            info_info6.SetActive(false);
         sound.Play();
     }
+
+    public void OpenInfo_Info4()
+    {
+        info_info.SetActive(false);
+        info_info2.SetActive(false);
+        if (info_info3 != null)
+            info_info3.SetActive(false);
+        if (info_info4 != null)
+            info_info4.SetActive(true);
+        if (info_info5 != null)
+            info_info5.SetActive(false);
+        if (info_info6 != null)
+            info_info6.SetActive(false);
+        sound.Play();
+    }
+
+    public void OpenInfo_Info5()
+    {
+        info_info.SetActive(false);
+        info_info2.SetActive(false);
+        if (info_info3 != null)
+            info_info3.SetActive(false);
+        if (info_info4 != null)
+            info_info4.SetActive(false);
+        if (info_info5 != null)
+            info_info5.SetActive(true);
+        if (info_info6 != null)
+            info_info6.SetActive(false);
+        sound.Play();
+    }
+
+    public void OpenInfo_Info6()
+    {
+        info_info.SetActive(false);
+        info_info2.SetActive(false);
+        if (info_info3 != null)
+            info_info3.SetActive(false);
+        if (info_info4 != null)
+            info_info4.SetActive(false);
+        if (info_info5 != null)
+            info_info5.SetActive(false);
+        if (info_info6 != null)
+            info_info6.SetActive(true);
+        sound.Play();
+    }
+
+
+    public void OpenInfo2_Info1()
+    {
+        info2_info.SetActive(true);
+        info2_info2.SetActive(false);
+        info2_info3.SetActive(false);
+        sound.Play();
+    }
+
+    public void OpenInfo2_Info2()
+    {
+        info2_info.SetActive(false);
+        info2_info2.SetActive(true);
+        info2_info3.SetActive(false);
+        sound.Play();
+    }
+
+    public void OpenInfo2_Info3()
+    {
+        info2_info.SetActive(false);
+        info2_info2.SetActive(false);
+        info2_info3.SetActive(true);
+        sound.Play();
+    }
+
+    //이전 패널로 가는 기능
 
     public void BackInfoPanel1()
     {
@@ -77,6 +225,29 @@ public class Panel : MonoBehaviour
     {
         infoPanel2.SetActive(false);
         panel.SetActive(true);
+        sound.Play();
+    }
+
+    //내부구조에서 이전 패널로 가는 기능
+
+    public void BackInfo2_Panel1()
+    {
+        info2_Panel.SetActive(false);
+        infoPanel2.SetActive(true);
+        sound.Play();
+    }
+
+    public void BackInfo2_Panel2()
+    {
+        info2_Panel2.SetActive(false);
+        infoPanel2.SetActive(true);
+        sound.Play();
+    }
+
+    public void BackInfo2_Panel3()
+    {
+        info2_Panel3.SetActive(false);
+        infoPanel2.SetActive(true);
         sound.Play();
     }
 }
